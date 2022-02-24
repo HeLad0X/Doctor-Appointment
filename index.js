cee7a77674fb6f6c60eabc2c361b8fc82c4dd9e1
Shiraz Ahmad <71230445+HeLad0X@users.noreply.github.com>
2022-02-24 13:17:18 +0530

    added page

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // hello there
+document.getElementById("login-button").addEventListener("click",function(){
+  let log_id = document.getElementById("input-id").value;
+  let log_pswd = document.getElementById("input-pswd").value;
+  if(log_id == "shirazhambi786@gmail.com" && log_pswd == "Passw@123"){
+    console.log(log_id);
+    console.log(log_pswd);
+
+  }
+  window.open("appointment-form.html")
+});
